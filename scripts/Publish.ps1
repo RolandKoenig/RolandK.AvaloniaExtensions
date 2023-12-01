b899434d2c47a7dd929a4a6c7f0c0ed1e022a5d2
Roland König <roland.koenig@rolandk.de>
2023-12-01 06:21:52 +0100

    Updated build settings

--- scripts/Publish.ps1
+++ scripts/Publish.ps1
@@ -6,4 +6,3 @@ Param(
 
 dotnet nuget push ../publish/RolandK.AvaloniaExtensions.$Version.nupkg --api-key $NugetApiKey --source https://api.nuget.org/v3/index.json
 dotnet nuget push ../publish/RolandK.AvaloniaExtensions.DependencyInjection.$Version.nupkg --api-key $NugetApiKey --source https://api.nuget.org/v3/index.json
-dotnet nuget push ../publish/RolandK.AvaloniaExtensions.FluentThemeDetection.$Version.nupkg --api-key $NugetApiKey --source https://api.nuget.org/v3/index.json
