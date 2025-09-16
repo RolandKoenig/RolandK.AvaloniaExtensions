55ba55460f7415fb1cd3ce40a903edb6a7c633ca
Roland König <roland.koenig@rolandk.de>
2025-09-16 08:12:37 +0800

    Added test view for ResponsiveGrid

---
+++ src/RolandK.AvaloniaExtensions.TestApp/Views/ResponsiveTwoColumnView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace RolandK.AvaloniaExtensions.TestApp.Views;
+
+public partial class ResponsiveTwoColumnView : UserControl
+{
+    public ResponsiveTwoColumnView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/RolandK.AvaloniaExtensions.TestApp/Views/ResponsiveTwoColumnViewModel.cs
@@ -0,0 +1,8 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RolandK.AvaloniaExtensions.TestApp.Views;
+
+public partial class ResponsiveTwoColumnViewModel : ObservableObject
+{
+    
+}
