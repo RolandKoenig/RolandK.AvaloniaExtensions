c0a84e6583bad50a77b5fba4a8577aa983d2009f
Roland König <roland.koenig@rolandk.de>
2026-05-15 13:49:30 +0200

    Updated build pipeline

--- scripts/workflows/publish.ps1
+++ scripts/workflows/publish.ps1
@@ -2,18 +2,16 @@
 Param(
     [Parameter(Mandatory = $false)]
     [string]$NugetApiKey,
-
-    [Parameter(Mandatory = $true)]
-    [string]$Version
 )
 
+# Get NuGet API key
 if ([string]::IsNullOrWhiteSpace($NugetApiKey)) {
     $NugetApiKey = $env:NUGET_API_KEY
 }
-
 if ([string]::IsNullOrWhiteSpace($NugetApiKey)) {
     Write-Error "NuGet API key was not provided. Pass -NugetApiKey or set the NUGET_API_KEY environment variable."
     exit 1
 }
 
+# Push all packages in the publish directory
 dotnet nuget push ../../publish/*.nupkg --api-key $NugetApiKey --source https://api.nuget.org/v3/index.json
