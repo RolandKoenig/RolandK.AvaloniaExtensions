f3a3cc129db9e226dd91e5fff6c7ca7eef1d780c
Roland König <roland.koenig@rolandk.de>
2026-05-15 13:09:04 +0200

    Updated actions and scripts

--- scripts/workflows/publish.ps1
+++ scripts/workflows/publish.ps1
@@ -1,13 +1,19 @@
 # definition of parameters
 Param(
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string]$NugetApiKey,
 
     [Parameter(Mandatory = $true)]
     [string]$Version
 )
 
-dotnet nuget push ../../publish/RolandK.AvaloniaExtensions.$Version.nupkg --api-key $NugetApiKey --source https://api.nuget.org/v3/index.json
-dotnet nuget push ../../publish/RolandK.AvaloniaExtensions.DependencyInjection.$Version.nupkg --api-key $NugetApiKey --source https://api.nuget.org/v3/index.json
-dotnet nuget push ../../publish/RolandK.AvaloniaExtensions.ExceptionHandling.$Version.nupkg --api-key $NugetApiKey --source https://api.nuget.org/v3/index.json
-dotnet nuget push ../../publish/RolandK.AvaloniaExtensions.ResponsiveControls.$Version.nupkg --api-key $NugetApiKey --source https://api.nuget.org/v3/index.json
+if ([string]::IsNullOrWhiteSpace($NugetApiKey)) {
+    $NugetApiKey = $env:NUGET_API_KEY
+}
+
+if ([string]::IsNullOrWhiteSpace($NugetApiKey)) {
+    Write-Error "NuGet API key was not provided. Pass -NugetApiKey or set the NUGET_API_KEY environment variable."
+    exit 1
+}
+
+dotnet nuget push ../../publish/*.nupkg --api-key $NugetApiKey --source https://api.nuget.org/v3/index.json
