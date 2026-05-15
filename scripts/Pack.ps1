f9fa12c2f67584c8dae7ee3132075ee2c0d00d1f
Roland König <roland.koenig@rolandk.de>
2026-05-15 10:22:06 +0200

    Automated publish (#88)
    * Updated LICENSE.md
    
    * Updated scripts, added set-version.ps1
    
    * Added publish action
    
    * Update publish-release.yml
    
    * Updated ci/cd

--- scripts/Pack.ps1
+++
@@ -1,9 +0,0 @@
-# Clear previous artifacts
-if (Test-Path "../publish")
-{
-	"Clearing ../publish"
-	Remove-Item -Path "../publish/*" -Recurse -Force;
-}
-
-# Build nuget packages
-dotnet pack -c Release -o ../publish "../RolandK.AvaloniaExtensions.sln"  /p:ContinuousIntegrationBuild=true /p:IncludeSymbols=true /p:EmbedUntrackedSources=true -p:SymbolPackageFormat=snupkg
---
+++ scripts/workflows/test.ps1
@@ -0,0 +1,1 @@
+dotnet test -c Release "../../RolandK.AvaloniaExtensions.sln"
